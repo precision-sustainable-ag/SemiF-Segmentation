8df694c403068e2e4e1c498ed9cf42a2457b435e
Matthew Kutugata <mattkutugata@gmail.com>
2024-11-23 15:39:47 -0500

    ignores and main updates

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ from src.preprocessing.grid_crop import main as grid_crop
 from src.preprocessing.train_val_test_split import main as train_val_test_split
 from src.preprocessing.remap_masks import main as remap_masks
 
+
+from src.train import main as train
+from src.viz_results import main as viz_results
+from src.data_stats import main as data_stats
+
 log = logging.getLogger(__name__)
 
 # Define a registry of tasks
@@ -17,6 +22,9 @@ TASK_REGISTRY = {
     "grid_crop": grid_crop,
     "train_val_test_split": train_val_test_split,
     "remap_masks": remap_masks,
+    "train": train,
+    "viz_results": viz_results,
+    "data_stats": data_stats,
     # Add more tasks here as needed
 }
 
