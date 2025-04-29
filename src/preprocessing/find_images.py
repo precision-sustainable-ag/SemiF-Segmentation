2664e628d0958f64d14e85692bfcae9597cd902a
Matthew Kutugata <mattkutugata@gmail.com>
2025-04-29 11:16:40 -0400

    refactor: sort image paths by their stem before writing to text file

--- src/preprocessing/find_images.py
+++ src/preprocessing/find_images.py
@@ -64,6 +64,8 @@ class DatasetManager:
     def write_data_2_text_file(self, data: List[Path], file_path: Path):
         """Write the list of paths to a text file."""
         # Remove duplicates
+        # Sort the list of image paths by the stem of the path
+        data = sorted(data, key=lambda x: x.stem)
         data = list(set(data))
         with file_path.open('w') as f:
             for item in data:
